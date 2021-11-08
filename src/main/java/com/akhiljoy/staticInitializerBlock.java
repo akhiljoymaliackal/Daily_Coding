5f892c126eb7e3a2aacf3fa2fd8d0b4b948c06ea
akhilmaliacal@gmail.com <Uzuymvw@2159>
2021-11-08 10:49:47 +0530

    1) Nov 8 2021 - Static Block

---
+++ src/main/java/com/akhiljoy/staticInitializerBlock.java
@@ -0,0 +1,26 @@
+package com.akhiljoy;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class staticInitializerBlock {
+    public static boolean flag;
+    public static int B;
+    public static int H;
+    static{
+        Scanner sc = new Scanner(System.in);
+        B = sc.nextInt();
+        H = sc.nextInt();
+        if (B>0 && H>0) flag=true;
+        else{
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
+    public static void main(String[] args) {
+        if(flag){
+            int area=B*H;
+            System.out.print(area);
+        }
+    }
+}

6272d07ff7cd5b68fd25690ba01f674e25ea9833
akhilmaliacal@gmail.com <Uzuymvw@2159>
2021-11-08 10:21:43 +0530

    1) Nov 8 2021 - Java End of File

---
+++ src/main/java/com/akhiljoy/EndOfFile.java
@@ -0,0 +1,13 @@
+package com.akhiljoy;
+
+import java.util.Scanner;
+
+public class EndOfFile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int line=0;
+        while (sc.hasNext()){
+            System.out.println(++line+" "+sc.nextLine());
+        }
+    }
+}
